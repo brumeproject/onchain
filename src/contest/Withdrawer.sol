8d24640fff2019624229f07557cc4da36f05fbbe
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-04-08 13:00:38 +0700

    d

--- src/contest/Withdrawer.sol
+++ src/contest/Withdrawer.sol
@@ -6,27 +6,26 @@ import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-// TODO: test
 contract Withdrawer is Ownable {
-
+    
     Votes public immutable votes;
-
+    
     address public immutable signer;
     address public immutable nuller;
-
+    
     IERC20 public immutable token;
-
+    
     uint256 public immutable delay;
-
+    
     mapping(bytes => Request) public requests;
-
+    
     struct Request {
         uint256 block;
         address target;
         uint256 amount;
         bool withdrawn;
     }
-
+    
     constructor(
         Votes votes_,
         address signer_,
@@ -34,41 +33,42 @@ contract Withdrawer is Ownable {
         IERC20 token_,
         uint256 delay_
     )
-        Ownable(msg.sender)
     {
-      votes = votes_;
-      signer = signer_;
-      nuller = nuller_;
-      token = token_;
-      delay = delay_;
+        votes = votes_;
+        signer = signer_;
+        nuller = nuller_;
+        token = token_;
+        delay = delay_;
     }
-
+    
+    function deposit(uint256 amount) public {
+        token.transferFrom(msg.sender, address(this), amount);
+    }
+    
     function request(address target, uint256 amount, bytes memory signature) public {
         bytes32 hash = keccak256(abi.encodePacked(target, amount));
-
+        
         address signer_ = ECDSA.recover(hash, signature);
         
         if (signer_ != signer)
             revert();
         if (requests[signature].withdrawn)
             revert();
-        if (votes.getPastVotes(signer, (block.number - 1)) < votes.getPastVotes(nuller, (block.number - 1)))
-            revert();
-
+        
         requests[signature] = Request({ block: block.number, target: target, amount: amount, withdrawn: false });
     }
-
+    
     function withdraw(bytes memory signature) public {
         Request storage request_ = requests[signature];
-
+        
         if (block.number < (request_.block + delay))
             revert();
         if (votes.getPastVotes(signer, (block.number - 1)) < votes.getPastVotes(nuller, (block.number - 1)))
             revert();
-
+        
         token.transfer(request_.target, request_.amount);
-
+        
         request_.withdrawn = true;
     }
-
+    
 }
